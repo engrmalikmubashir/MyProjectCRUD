d0bba574e762c2e3208b507228fd6c0090c8098e
Muhammad Mubashir <57788343+engrmalikmubashir@users.noreply.github.com>
2020-04-10 22:37:16 +0500

    Add project files.

---
+++ MyProject/Migrations/20200410173239_basicDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Migrations
+{
+    public partial class basicDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Continent = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
---
+++ MyProject/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class Country
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "This field is required!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is required!")]
+        public string Continent { get; set; }
+    }
+}
